1026e4529b88ee68c73d85e1f5190a7467636ff5
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-03-07 02:24:12 +0700

    Update checker.js

--- checker.js
+++ checker.js
@@ -1,5 +1,5 @@
 for(i=1;i<=13;i++){
-str = 'choice'+i+ '-'+Math.floor(Math.random() * Math.floor(3) + 3)
-console.log(str)
-document.getElementById(str).checked = true;
+  str = 'choice'+i+ '-'+Math.floor(Math.random() * Math.floor(3) + 3)
+  console.log(str)
+  document.getElementById(str).checked = true;
 }
